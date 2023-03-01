1752cceb4bd554809170a4af82ccd76950d837d6
jokswer <jokswer@gmail.com>
2023-03-01 22:38:44 +0700

    feat: add camera transform

---
+++ Assets/Scripts/CameraTransform.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraTransform : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _lerpRate;
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _lerpRate);
+    }
+}
